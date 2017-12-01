e6d37afcead1000a58444521c0c710b55333dd95
Ben Ely <ben.ely@h4bmanchester.com>
2017-12-01 14:08:56 +0000

    Setup ES6 and modulise challenges in to app folder

---
+++ app/day-1.js
@@ -0,0 +1,9 @@
+// INVERSE CAPTCHA
+
+/* The captcha requires you to review a sequence of digits (your puzzle input)
+	and find the sum of all digits that match the next digit in the list. The list
+	is circular, so the digit after the last digit is the first digit in the list.
+*/
+export default function(sequence) {
+	return 1;
+};
--- test/day1.js
+++ test/day1.js
@@ -1,11 +1,36 @@
-var adventOfCode = require('../app.js'),
-	should = require('should');
+import should from 'should';
+import captcha from '../app/day-1.js';
 
-describe('Day 1', function() {
-	describe('When passing a value to my function', function() {
-		it('should return the same value', function() {
-			var aoc = adventOfCode.Day1(5)
-			aoc.should.equal(5);
+describe('INVERSE CAPTCHA', function() {
+	describe('When passed a captcha of 1122', function() {
+		it('should return an answer of 3', function() {
+			const value = captcha(1122);
+
+			value.should.equal(3);
+		});
+	});
+
+	describe('When passed a captcha of 1111', function() {
+		it('should return an answer of 4', function() {
+			const value = captcha(1111);
+
+			value.should.equal(4);
+		});
+	});
+
+	describe('When passed a captcha of 1234', function() {
+		it('should return an answer of 0', function() {
+			const value = captcha(1234);
+
+			value.should.equal(0);
+		});
+	});
+
+	describe('When passed a captcha of 91212129', function() {
+		it('should return an answer of 9', function() {
+			const value = captcha(91212129);
+
+			value.should.equal(9);
 		});
 	});
 });
