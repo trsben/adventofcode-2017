86162b1b7c5eeeffcf98af3bf27ee05c78e0dd96
Martin Hymers <martin.hymers@havasww.com>
2017-12-01 17:36:10 +0000

    Added solution to part 2 of day 1

--- env/martin/day-1_part2.js
+++ env/martin/day-1_part2.js
@@ -1,29 +1,27 @@
 // INVERSE CAPTCHA
 
 /* The captcha requires you to review a sequence of digits (your puzzle input)
-	and find the sum of all digits that match the next digit in the list. The list
-	is circular, so the digit after the last digit is the first digit in the list.
-*/
-const captcha = function(sequence) {
-	var total = 0;
+ and find the sum of all digits that match the next digit in the list. The list
+ is circular, so the digit after the last digit is the first digit in the list.
+ */
+const captcha = function (sequence) {
+    var total = 0;
+    var offset = sequence.length / 2;
+    var sequenceExtended = sequence + sequence; // prevent need for wrap around
 
-	for(var i = 0; i < sequence.length; i++) {
-		var index1 = i;
-		var index2 = 0;
+    for (var i = 0; i < sequence.length; i++) {
+        var index1 = i;
+        var index2 = i + offset;
 
-		if(index1+1 !== sequence.length) {
-			index2 = i + 1;
-		}
+        var compare1 = sequenceExtended[index1];
+        var compare2 = sequenceExtended[index2];
 
-		var compare1 = sequence[index1];
-		var compare2 = sequence[index2];
+        if (compare1 === compare2) {
+            total += Number(compare1);
+        }
+    }
 
-		if(compare1 === compare2) {
-			total += Number(compare1);
-		}
-	}
-
-	return total;
+    return total;
 };
 
 module.exports = captcha;
