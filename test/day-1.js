f2400d0d7d122894eb248b31b8d3e0d9aa6122f6
Martin Hymers <martin.hymers@havasww.com>
2017-12-01 14:42:07 +0000

    day1 problem 1 - basic none fancy solution to pass the tests

--- test/day-1.js
+++ test/day-1.js
@@ -4,7 +4,7 @@ import captcha from '../app/day-1.js';
 describe('INVERSE CAPTCHA', function() {
 	describe('When passed a captcha of 1122', function() {
 		it('should return an answer of 3', function() {
-			const value = captcha(1122);
+			const value = captcha('1122');
 
 			value.should.equal(3);
 		});
@@ -12,7 +12,7 @@ describe('INVERSE CAPTCHA', function() {
 
 	describe('When passed a captcha of 1111', function() {
 		it('should return an answer of 4', function() {
-			const value = captcha(1111);
+			const value = captcha('1111');
 
 			value.should.equal(4);
 		});
@@ -20,7 +20,7 @@ describe('INVERSE CAPTCHA', function() {
 
 	describe('When passed a captcha of 1234', function() {
 		it('should return an answer of 0', function() {
-			const value = captcha(1234);
+			const value = captcha('1234');
 
 			value.should.equal(0);
 		});
@@ -28,7 +28,7 @@ describe('INVERSE CAPTCHA', function() {
 
 	describe('When passed a captcha of 91212129', function() {
 		it('should return an answer of 9', function() {
-			const value = captcha(91212129);
+			const value = captcha('91212129');
 
 			value.should.equal(9);
 		});
